78b35cfe0dbf04ab99a2b28d7aa04a405791c2f8
Elifcesr <elif_cesr@outlook.com>
2025-05-11 00:32:55 +0300

    Melee attack added to Player1

---
+++ Assets/Scripts/PlayerMelee.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMelee : MonoBehaviour
+{
+    public Transform attackOrigin;
+    public float attackRadius = 1f;
+    public LayerMask enemyMask;
+
+    public int attackDamage = 25;
+
+    public float cooldownTime = .5f;
+    private float cooldownTimer = 0f;
+
+    private void Update()
+    {
+        if (cooldownTimer <= 0)
+        {
+            if (Input.GetMouseButton(0)) // left click
+            {
+                Collider2D[] enemiesInRange = Physics2D.OverlapCircleAll(attackOrigin.position, attackRadius, enemyMask);
+                foreach (var enemy in enemiesInRange)
+                {
+                    enemy.GetComponent<EnemyHealth>().TakeDamage(attackDamage);
+                }
+
+                cooldownTimer = cooldownTime;
+            }
+        }
+        else
+        {
+            cooldownTimer -= Time.deltaTime;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(attackOrigin.position, attackRadius);
+    }
+}
