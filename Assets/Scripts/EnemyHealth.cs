19fa0541a679dbe03335afbdc4a3d6e8d5528033
Elifcesr <elif_cesr@outlook.com>
2025-05-30 21:31:23 +0300

    Health bars are added to enemies.

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,36 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class EnemyHealth : MonoBehaviour
 {
     public int maxHealth = 3;
     public int currentHealth;
 
-    // Start is called before the first frame update
+    public Slider healthSlider; // slider
+
     void Start()
     {
-        currentHealth = maxHealth; //starts the game with full health
+        currentHealth = maxHealth;
+
+        if (healthSlider != null)
+        {
+            healthSlider.maxValue = maxHealth;
+            healthSlider.value = currentHealth;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int amount)
     {
-        
-    }
+        currentHealth -= amount;
 
-    public void TakeDamage(int amount) //amount: an integer for how much damage to take
-    {
-        currentHealth -= amount; // enemy's currentHealth goes down by the amount of damage that is dealt.
-        if(currentHealth <= 0)
+        if (healthSlider != null)
+        {
+            healthSlider.value = currentHealth;
+        }
+
+        if (currentHealth <= 0)
         {
             Die();
         }
     }
+
     void Die()
     {
         GameManager.instance.EnemyKilled();
-
         Destroy(gameObject);
     }
 }
